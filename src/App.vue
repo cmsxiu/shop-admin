76384786f5dd25fd18c0a372d509a6cfdc7f5b31
Mr.Shen <cmsxiu@qq.com>
2019-10-18 07:42:31 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <mt-header fixed title="女神秀-做你自己的女神"></mt-header>
+    <router-view />
+    <h3>测试内容</h3>
+    <mt-tabbar>
+      <mt-tab-item id="首页">
+		<i class="icon iconfont" slot="icon">&#xe6b8;</i>
+        首页
+      </mt-tab-item>
+      <mt-tab-item id="分类">
+		<i class="icon iconfont" slot="icon">&#xe729;</i>
+        分类
+      </mt-tab-item>
+      <mt-tab-item id="购物车">
+		<i class="icon iconfont" slot="icon">&#xe7d8;</i>
+        购物车
+      </mt-tab-item>
+      <mt-tab-item id="我的">
+		<i class="icon iconfont" slot="icon">&#xe78b;</i>
+        我的
+      </mt-tab-item>
+    </mt-tabbar>
+  </div>
+</template>
+<script>
+	export default {
+		name: 'app',
+		data(){
+			return {}
+		},
+		methods: {
+			say: () => {
+				var a = 123
+				var b = 234
+				console.table({a,b})
+			}
+		},
+		created() {
+			this.say()
+		}
+	}
+</script>
+<style>
+	@import url("./assets/icon/iconfont.css");
+	h3 {
+	  padding-top: 40px;
+	}
+
+</style>
---
+++ src/views/Header.vue
@@ -0,0 +1,11 @@
+<template>
+	<mt-header fixed title="这里是头部"></mt-header>
+</template>
+<script type="text/javascript">
+	export default {
+
+	}
+</script>
+<style type="text/css">
+	
+</style>
